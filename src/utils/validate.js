6c2ea0ba0fb0680049c549da8b61c7722f9a957e
Auroka <auroka@163.com>
2020-06-19 16:51:31 +0800

    加入手机号、邮箱、密码校验

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,4 +1,3 @@
-
 /**
  * @param {string} path
  * @returns {Boolean}
@@ -7,3 +6,29 @@ export function isExternal(path) {
   return /^(https?:|mailto:|tel:)/.test(path)
 }
 
+/**
+ * @param {string} mobile
+ * @returns {Boolean}
+ */
+export function isMobile(mobile) {
+  return /^1[0-9]{10}$/.test(mobile)
+}
+
+/**
+ * @param {string} email
+ * @returns {Boolean}
+ */
+export function isEmail(email) {
+  return /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((.[a-zA-Z0-9_-]{2,3}){1,2})$/.test(
+    email
+  )
+}
+
+/**
+ * 校验密码 只能包含数字、字母、下划线
+ * @param {string} password
+ * @returns {Boolean}
+ */
+export function validPassword(password) {
+  return /^[a-zA-Z0-9_]*$/.test(password)
+}
