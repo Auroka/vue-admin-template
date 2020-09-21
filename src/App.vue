17cf932cbc00ef4b0d2915fc28266a1081350580
Auroka <auroka@163.com>
2020-09-21 13:26:42 +0800

    测试husky

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
+<!--
+ * @Description: 
+ * @Author: lxd
+ * @Date: 2020-06-08 17:31:42
+ * @LastEditTime: 2020-09-21 13:26:32
+-->
 <template>
   <div id="app">
     <router-view />
   </div>
 </template>
-
 <script>
 export default {
   name: 'App'
