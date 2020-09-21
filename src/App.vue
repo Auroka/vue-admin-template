4b5d32111f87bc25c93bffaf686a89235c5de606
Auroka <auroka@163.com>
2020-09-21 14:33:34 +0800

    style: 删除一行

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,13 @@
  * @Description: 
  * @Author: lxd
  * @Date: 2020-06-08 17:31:42
- * @LastEditTime: 2020-09-21 13:30:09
+ * @LastEditTime: 2020-09-21 14:32:58
 -->
 <template>
   <div id="app">
     <router-view />
   </div>
 </template>
-
 <script>
 export default {
   name: 'App'
